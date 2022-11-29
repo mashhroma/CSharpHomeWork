c4c93c449507906aed1ba78ff53191e471348dc6
Mashhroma <mashh@mail.ru>
2022-11-29 22:02:52 +0300

    task08 is improve

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,22 +2,40 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.Write("Введите, пожалуйста, число: ");
+int Number = 1;
+Console.Write("Введите, пожалуйста, число больше единицы: ");
 int maxNumber = int.Parse(Console.ReadLine()!);
 
-int Number = 1;
+while (maxNumber < 2)
+{
+    if (maxNumber == 0)
+    {
+    Console.WriteLine("Введенное число равно 0.");
+    Console.Write("Введите, пожалуйста, число больше единицы: ");
+    }
+    else if (maxNumber ==1)
+    {
+    Console.WriteLine($"Введенное число равно 1.");
+    Console.Write("Введите, пожалуйста, число больше единицы: ");
+    }
+    else
+    {
+    Console.WriteLine($"Введенное число {maxNumber} - меньше единицы.");
+    Console.Write("Введите, пожалуйста, число больше единицы: ");
+    }
+    maxNumber = int.Parse(Console.ReadLine()!);
+}
 
-Console.Write($"Все чётные числа от 1 до {maxNumber}:    ");
-
+Console.Write($"Все чётные числа от 1 до введенного числа {maxNumber}:    ");
 while (Number <= maxNumber)
 {
     if (Number%2 == 0)
     {
-        Console.Write($"{Number}, ");
-        Number++;
+         Console.Write($"{Number}, ");
+         Number++;
     }
     else
     {
-        Number++;
+         Number++;
     }
 }

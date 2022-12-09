9e477a1b00209b23c9942f5f0467e01613843c20
Mashhroma <mashh@mail.ru>
2022-12-09 22:08:08 +0300

    task 27

---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int FiguresSum(int number)
+{
+    int sum = 0;
+    if (number < 0) number = -number;
+    for (int i = 0; number > 0; i++)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+        //Console.WriteLine(i + "|" + sum);
+    }
+    return sum;
+}
+
+Console.WriteLine("Данная программа суммирует цифры в заданном числе.");
+
+Console.Write("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр в числе {num} равна: {FiguresSum(num)}.");

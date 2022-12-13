ca93989e4f59426c080b9f49071ac082708b0b07
Mashhroma <mashh@mail.ru>
2022-12-13 22:02:48 +0300

    task 34

---
+++ task34/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray (int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(minValue,maxValue);
+    return array;
+}
+
+void PrintArray (int[] collection)
+{
+    foreach (var item in collection) Console.Write (item + " ");
+}
+
+int CountEvenNums (int[] collection)
+{
+    int count = 0;
+    foreach (var item in collection)
+    {
+        if (item%2 == 0) count++;
+    }
+    return count;
+}
+
+int[] myArray = CreateArray(10, 100, 1000);
+
+PrintArray(myArray);
+
+Console.WriteLine();
+Console.WriteLine($"Количество нечетных чисел в массиве: {CountEvenNums(myArray)}.");
